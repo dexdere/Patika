e70c23c6957b218e4cd43473a36ead567a1709c8
Batuhan <batuhanmunger@hotmail.com>
2021-05-03 17:19:49 +0300

    .

--- JavaScript/Odev1/clock.js
+++ JavaScript/Odev1/clock.js
@@ -11,19 +11,19 @@ function showTime() {
   var day = date.getDay().toString();
 
   if ((day = 1)) {
-    day = "Pazar";
-  } else if ((day = 2)) {
     day = "Pazartesi";
-  } else if ((day = 3)) {
+  } else if ((day = 2)) {
     day = "Salı";
-  } else if ((day = 4)) {
+  } else if ((day = 3)) {
     day = "Çarşamba";
-  } else if ((day = 5)) {
+  } else if ((day = 4)) {
     day = "Perşembe";
-  } else if ((day = 6)) {
+  } else if ((day = 5)) {
     day = "Cuma";
-  } else if ((day = 7)) {
+  } else if ((day = 6)) {
     day = "Cumartesi";
+  } else if ((day = 7)) {
+    day = "Pazar";
   }
 
   hour = hour < 10 ? "0" + hour : hour;
